bbecd6d51350dedf817f2820aff22741afde1b19
netsanetw <167537781+netsanetw@users.noreply.github.com>
2025-01-10 22:08:09 +0300

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,36 @@
+const display = document.getElementById('display');
+const clear = document.getElementById('clear');
+const equal = document.getElementById('equals');
+
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+
+function calculate() {
+    try {
+       display.value = eval(display.value);
+    } catch {
+        display.value = "error";
+    }
+}
+
+document.querySelectorAll('.btn').forEach(button => {
+    button.addEventListener('click', () => {
+        const value = button.getAttribute('data-value');
+        if (value) {
+            appendToDisplay(value);
+        }
+    });
+});
+
+
+clear.addEventListener('click', clearDisplay);
+
+
+equal.addEventListener('click', calculate);
